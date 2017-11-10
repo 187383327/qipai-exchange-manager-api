9fd70e67cc07cde43dca492aec0030f4d041668b
xuzewei <xuzewei_2012@126.com>
2017-11-10 11:20:48 +0800

    ignite-utils升级至v1.0.2

--- src/main/java/com/github/microprograms/qipai_exchange_manager_api/public_api/Goods_Add_Api.java
+++ src/main/java/com/github/microprograms/qipai_exchange_manager_api/public_api/Goods_Add_Api.java
@@ -1,10 +1,16 @@
 package com.github.microprograms.qipai_exchange_manager_api.public_api;
 
-import com.github.microprograms.micro_entity_definition_runtime.annotation.Comment;
+import java.sql.Connection;
+import java.util.UUID;
+
+import com.github.microprograms.ignite_utils.IgniteUtils;
+import com.github.microprograms.ignite_utils.sql.dml.InsertSql;
 import com.github.microprograms.micro_api_runtime.annotation.MicroApiAnnotation;
-import com.github.microprograms.micro_api_runtime.model.Response;
 import com.github.microprograms.micro_api_runtime.model.Request;
+import com.github.microprograms.micro_api_runtime.model.Response;
+import com.github.microprograms.micro_entity_definition_runtime.annotation.Comment;
 import com.github.microprograms.micro_entity_definition_runtime.annotation.Required;
+import com.github.microprograms.qipai_exchange_manager_api.utils.Consts;
 
 @Comment(value = "商品 - 添加新商品")
 @MicroApiAnnotation(type = "read", version = "v1.0.6")
@@ -12,15 +18,28 @@ public class Goods_Add_Api {
 
     public static Response execute(Request request) throws Exception {
         Req req = (Req) request;
+        try (Connection conn = IgniteUtils.getConnection(Consts.jdbc_url)) {
+            Goods goods = new Goods();
+            goods.setId(UUID.randomUUID().toString());
+            goods.setCategoryId(req.getCategoryId());
+            goods.setName(req.getName());
+            goods.setPrice(req.getPrice());
+            goods.setVipPrice(req.getVipPrice());
+            goods.setGoldVipPrice(req.getGoldVipPrice());
+            goods.setReorder(req.getReorder());
+            goods.setStock(req.getStock());
+            goods.setPictures(req.getPictures());
+            goods.setDetail(req.getDetail());
+            goods.setDtCreate(System.currentTimeMillis());
+            conn.createStatement().executeUpdate(InsertSql.build(goods));
+        }
         Response resp = new Response();
         return resp;
     }
 
     public static class Req extends Request {
 
-        @Comment(value = "商品类别编号")
-        @Required(value = true)
-        private Integer categoryId;
+        @Comment(value = "商品类别编号") @Required(value = true) private Integer categoryId;
 
         public Integer getCategoryId() {
             return categoryId;
@@ -30,9 +49,7 @@ public class Goods_Add_Api {
             this.categoryId = categoryId;
         }
 
-        @Comment(value = "商品名")
-        @Required(value = true)
-        private String name;
+        @Comment(value = "商品名") @Required(value = true) private String name;
 
         public String getName() {
             return name;
@@ -42,9 +59,7 @@ public class Goods_Add_Api {
             this.name = name;
         }
 
-        @Comment(value = "商品价格(元宝)")
-        @Required(value = true)
-        private Integer price;
+        @Comment(value = "商品价格(元宝)") @Required(value = true) private Integer price;
 
         public Integer getPrice() {
             return price;
@@ -54,9 +69,7 @@ public class Goods_Add_Api {
             this.price = price;
         }
 
-        @Comment(value = "会员价格(元宝)")
-        @Required(value = true)
-        private Integer vipPrice;
+        @Comment(value = "会员价格(元宝)") @Required(value = true) private Integer vipPrice;
 
         public Integer getVipPrice() {
             return vipPrice;
@@ -66,9 +79,7 @@ public class Goods_Add_Api {
             this.vipPrice = vipPrice;
         }
 
-        @Comment(value = "钻石会员(元宝)")
-        @Required(value = true)
-        private Integer goldVipPrice;
+        @Comment(value = "钻石会员(元宝)") @Required(value = true) private Integer goldVipPrice;
 
         public Integer getGoldVipPrice() {
             return goldVipPrice;
@@ -78,9 +89,7 @@ public class Goods_Add_Api {
             this.goldVipPrice = goldVipPrice;
         }
 
-        @Comment(value = "排序")
-        @Required(value = true)
-        private Integer reorder;
+        @Comment(value = "排序") @Required(value = true) private Integer reorder;
 
         public Integer getReorder() {
             return reorder;
@@ -90,9 +99,7 @@ public class Goods_Add_Api {
             this.reorder = reorder;
         }
 
-        @Comment(value = "库存")
-        @Required(value = true)
-        private Integer stock;
+        @Comment(value = "库存") @Required(value = true) private Integer stock;
 
         public Integer getStock() {
             return stock;
@@ -102,9 +109,7 @@ public class Goods_Add_Api {
             this.stock = stock;
         }
 
-        @Comment(value = "商品主图(JsonArray)")
-        @Required(value = true)
-        private String pictures;
+        @Comment(value = "商品主图(JsonArray)") @Required(value = true) private String pictures;
 
         public String getPictures() {
             return pictures;
@@ -114,9 +119,7 @@ public class Goods_Add_Api {
             this.pictures = pictures;
         }
 
-        @Comment(value = "详情(富文本)")
-        @Required(value = true)
-        private String detail;
+        @Comment(value = "详情(富文本)") @Required(value = true) private String detail;
 
         public String getDetail() {
             return detail;
--- src/main/java/com/github/microprograms/qipai_exchange_manager_api/public_api/Goods_QueryList_Api.java
+++ src/main/java/com/github/microprograms/qipai_exchange_manager_api/public_api/Goods_QueryList_Api.java
@@ -1,10 +1,19 @@
 package com.github.microprograms.qipai_exchange_manager_api.public_api;
 
-import com.github.microprograms.micro_entity_definition_runtime.annotation.Comment;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import com.github.microprograms.ignite_utils.IgniteUtils;
+import com.github.microprograms.ignite_utils.sql.dml.ComplexCondition;
+import com.github.microprograms.ignite_utils.sql.dml.Condition;
+import com.github.microprograms.ignite_utils.sql.dml.SelectSql;
+import com.github.microprograms.ignite_utils.sql.dml.Where;
 import com.github.microprograms.micro_api_runtime.annotation.MicroApiAnnotation;
-import com.github.microprograms.micro_api_runtime.model.Response;
 import com.github.microprograms.micro_api_runtime.model.Request;
+import com.github.microprograms.micro_api_runtime.model.Response;
+import com.github.microprograms.micro_entity_definition_runtime.annotation.Comment;
 import com.github.microprograms.micro_entity_definition_runtime.annotation.Required;
+import com.github.microprograms.qipai_exchange_manager_api.utils.Consts;
 
 @Comment(value = "商品 - 查询列表")
 @MicroApiAnnotation(type = "read", version = "v1.0.6")
@@ -13,14 +22,27 @@ public class Goods_QueryList_Api {
     public static Response execute(Request request) throws Exception {
         Req req = (Req) request;
         Resp resp = new Resp();
+        try (Connection conn = IgniteUtils.getConnection(Consts.jdbc_url)) {
+            SelectSql selectSql = new SelectSql();
+            selectSql.tableName(Goods.class.getSimpleName());
+            selectSql.where(buildFinalCondition(req));
+            selectSql.pager(IgniteUtils.buildPager(req.getPageIndex(), req.getPageSize()));
+            ResultSet rs = conn.createStatement().executeQuery(selectSql.build());
+            resp.setData(IgniteUtils.getJavaObjectList(rs, Goods.class));
+        }
         return resp;
+    }
+
+    private static ComplexCondition buildFinalCondition(Req req) {
+        ComplexCondition dtCreate = Where.and(Condition.build("dtCreate>", req.getSearchBeginTimestamp()), Condition.build("dtCreate<", req.getSearchEndTimestamp()));
+        ComplexCondition stock = Where.and(Condition.build("stock>", req.getSearchMinStock()), Condition.build("stock<", req.getSearchMaxStock()));
+        Condition keyword = Condition.build("name like", "%" + req.getSearchKeyword() + "%");
+        return Where.and(dtCreate, stock, keyword);
     }
 
     public static class Req extends Request {
 
-        @Comment(value = "页码(从0开始)")
-        @Required(value = true)
-        private Integer pageIndex;
+        @Comment(value = "页码(从0开始)") @Required(value = true) private Integer pageIndex;
 
         public Integer getPageIndex() {
             return pageIndex;
@@ -30,9 +52,7 @@ public class Goods_QueryList_Api {
             this.pageIndex = pageIndex;
         }
 
-        @Comment(value = "页大小")
-        @Required(value = true)
-        private Integer pageSize;
+        @Comment(value = "页大小") @Required(value = true) private Integer pageSize;
 
         public Integer getPageSize() {
             return pageSize;
@@ -42,9 +62,7 @@ public class Goods_QueryList_Api {
             this.pageSize = pageSize;
         }
 
-        @Comment(value = "搜索 - 关键字")
-        @Required(value = false)
-        private String searchKeyword;
+        @Comment(value = "搜索 - 关键字") @Required(value = false) private String searchKeyword;
 
         public String getSearchKeyword() {
             return searchKeyword;
@@ -54,9 +72,7 @@ public class Goods_QueryList_Api {
             this.searchKeyword = searchKeyword;
         }
 
-        @Comment(value = "搜索 - 开始时间戳")
-        @Required(value = false)
-        private Long searchBeginTimestamp;
+        @Comment(value = "搜索 - 开始时间戳") @Required(value = false) private Long searchBeginTimestamp;
 
         public Long getSearchBeginTimestamp() {
             return searchBeginTimestamp;
@@ -66,9 +82,7 @@ public class Goods_QueryList_Api {
             this.searchBeginTimestamp = searchBeginTimestamp;
         }
 
-        @Comment(value = "搜索 - 结束时间戳")
-        @Required(value = false)
-        private Long searchEndTimestamp;
+        @Comment(value = "搜索 - 结束时间戳") @Required(value = false) private Long searchEndTimestamp;
 
         public Long getSearchEndTimestamp() {
             return searchEndTimestamp;
@@ -78,9 +92,7 @@ public class Goods_QueryList_Api {
             this.searchEndTimestamp = searchEndTimestamp;
         }
 
-        @Comment(value = "搜索 - 最小库存")
-        @Required(value = false)
-        private Integer searchMinStock;
+        @Comment(value = "搜索 - 最小库存") @Required(value = false) private Integer searchMinStock;
 
         public Integer getSearchMinStock() {
             return searchMinStock;
@@ -90,9 +102,7 @@ public class Goods_QueryList_Api {
             this.searchMinStock = searchMinStock;
         }
 
-        @Comment(value = "搜索 - 最大库存")
-        @Required(value = false)
-        private Integer searchMaxStock;
+        @Comment(value = "搜索 - 最大库存") @Required(value = false) private Integer searchMaxStock;
 
         public Integer getSearchMaxStock() {
             return searchMaxStock;
@@ -105,9 +115,7 @@ public class Goods_QueryList_Api {
 
     public static class Resp extends Response {
 
-        @Comment(value = "商品列表")
-        @Required(value = true)
-        private java.util.List<Goods> data;
+        @Comment(value = "商品列表") @Required(value = true) private java.util.List<Goods> data;
 
         public java.util.List<Goods> getData() {
             return data;
@@ -117,9 +125,7 @@ public class Goods_QueryList_Api {
             this.data = data;
         }
 
-        @Comment(value = "分页")
-        @Required(value = true)
-        private Pager pager;
+        @Comment(value = "分页") @Required(value = true) private Pager pager;
 
         public Pager getPager() {
             return pager;
