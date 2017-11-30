5adea915185a30c729a3d65a9746c39ccacb0a39
xuzewei <xuzewei_2012@126.com>
2017-11-30 13:42:02 +0800

    come true

--- src/main/java/com/github/microprograms/qipai_exchange_manager_api/public_api/ChoiceBanner_QueryAll_Api.java
+++ src/main/java/com/github/microprograms/qipai_exchange_manager_api/public_api/ChoiceBanner_QueryAll_Api.java
@@ -1,10 +1,18 @@
 package com.github.microprograms.qipai_exchange_manager_api.public_api;
 
-import com.github.microprograms.micro_entity_definition_runtime.annotation.Comment;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import com.github.microprograms.ignite_utils.IgniteUtils;
+import com.github.microprograms.ignite_utils.sql.dml.Condition;
+import com.github.microprograms.ignite_utils.sql.dml.SelectSql;
+import com.github.microprograms.ignite_utils.sql.dml.Sort;
 import com.github.microprograms.micro_api_runtime.annotation.MicroApiAnnotation;
-import com.github.microprograms.micro_api_runtime.model.Response;
 import com.github.microprograms.micro_api_runtime.model.Request;
+import com.github.microprograms.micro_api_runtime.model.Response;
+import com.github.microprograms.micro_entity_definition_runtime.annotation.Comment;
 import com.github.microprograms.micro_entity_definition_runtime.annotation.Required;
+import com.github.microprograms.qipai_exchange_manager_api.utils.Consts;
 
 @Comment(value = "会员专区Banner - 查询全部")
 @MicroApiAnnotation(type = "read", version = "v1.0.28")
@@ -12,14 +20,21 @@ public class ChoiceBanner_QueryAll_Api {
 
     public static Response execute(Request request) throws Exception {
         Resp resp = new Resp();
+        try (Connection conn = IgniteUtils.getConnection(Consts.jdbc_url)) {
+            String finalCondition = buildFinalCondition();
+            ResultSet rs = conn.createStatement().executeQuery(new SelectSql(Banner.class).where(finalCondition).sorts(Sort.asc("reorder")).build());
+            resp.setData(IgniteUtils.getJavaObjectList(rs, Banner.class));
+        }
         return resp;
+    }
+
+    private static String buildFinalCondition() {
+        return Condition.build("type=", "2").toString();
     }
 
     public static class Resp extends Response {
 
-        @Comment(value = "Banner列表(全部)")
-        @Required(value = true)
-        private java.util.List<Banner> data;
+        @Comment(value = "Banner列表(全部)") @Required(value = true) private java.util.List<Banner> data;
 
         public java.util.List<Banner> getData() {
             return data;
--- src/main/java/com/github/microprograms/qipai_exchange_manager_api/public_api/ChoiceBanner_UpdateAll_Api.java
+++ src/main/java/com/github/microprograms/qipai_exchange_manager_api/public_api/ChoiceBanner_UpdateAll_Api.java
@@ -1,10 +1,19 @@
 package com.github.microprograms.qipai_exchange_manager_api.public_api;
 
-import com.github.microprograms.micro_entity_definition_runtime.annotation.Comment;
+import java.sql.Connection;
+import java.util.UUID;
+
+import com.github.microprograms.ignite_utils.IgniteUtils;
+import com.github.microprograms.ignite_utils.sql.dml.Condition;
+import com.github.microprograms.ignite_utils.sql.dml.DeleteSql;
+import com.github.microprograms.ignite_utils.sql.dml.InsertSql;
 import com.github.microprograms.micro_api_runtime.annotation.MicroApiAnnotation;
-import com.github.microprograms.micro_api_runtime.model.Response;
 import com.github.microprograms.micro_api_runtime.model.Request;
+import com.github.microprograms.micro_api_runtime.model.Response;
+import com.github.microprograms.micro_entity_definition_runtime.annotation.Comment;
 import com.github.microprograms.micro_entity_definition_runtime.annotation.Required;
+import com.github.microprograms.qipai_exchange_manager_api.public_api.Banner_QueryAll_Api.Resp;
+import com.github.microprograms.qipai_exchange_manager_api.utils.Consts;
 
 @Comment(value = "会员专区Banner - 更新全部")
 @MicroApiAnnotation(type = "read", version = "v1.0.28")
@@ -12,15 +21,28 @@ public class ChoiceBanner_UpdateAll_Api {
 
     public static Response execute(Request request) throws Exception {
         Req req = (Req) request;
-        Response resp = new Response();
+        Resp resp = new Resp();
+        try (Connection conn = IgniteUtils.getConnection(Consts.jdbc_url)) {
+            conn.createStatement().executeUpdate(new DeleteSql(Banner.class).where(Condition.build("type=", 2)).build());
+            for (Banner x : req.getBanners()) {
+                if (x.getReorder() == null) {
+                    continue;
+                }
+                com.github.microprograms.qipai_exchange_core.model.Banner newBanner = new com.github.microprograms.qipai_exchange_core.model.Banner();
+                newBanner.setId(UUID.randomUUID().toString());
+                newBanner.setType(2);
+                newBanner.setReorder(x.getReorder());
+                newBanner.setUrl(x.getUrl());
+                newBanner.setDtCreate(System.currentTimeMillis());
+                conn.createStatement().executeUpdate(InsertSql.build(newBanner));
+            }
+        }
         return resp;
     }
 
     public static class Req extends Request {
 
-        @Comment(value = "Banner列表(全部)")
-        @Required(value = true)
-        private java.util.List<Banner> banners;
+        @Comment(value = "Banner列表(全部)") @Required(value = true) private java.util.List<Banner> banners;
 
         public java.util.List<Banner> getBanners() {
             return banners;
