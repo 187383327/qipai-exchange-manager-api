c266fb0512582ce6d13b2237307de87930d6f66b
xuzewei <xuzewei_2012@126.com>
2017-12-20 15:18:01 +0800

    RoomCardOrder_QueryList_Api.java

--- src/main/java/com/github/microprograms/qipai_exchange_manager_api/public_api/RoomCardOrder_QueryList_Api.java
+++ src/main/java/com/github/microprograms/qipai_exchange_manager_api/public_api/RoomCardOrder_QueryList_Api.java
@@ -1,10 +1,23 @@
 package com.github.microprograms.qipai_exchange_manager_api.public_api;
 
-import com.github.microprograms.micro_entity_definition_runtime.annotation.Comment;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import com.github.microprograms.ignite_utils.IgniteUtils;
+import com.github.microprograms.ignite_utils.sql.dml.ComplexCondition;
+import com.github.microprograms.ignite_utils.sql.dml.Condition;
+import com.github.microprograms.ignite_utils.sql.dml.LikeCondition;
+import com.github.microprograms.ignite_utils.sql.dml.PagerRequest;
+import com.github.microprograms.ignite_utils.sql.dml.PagerResponse;
+import com.github.microprograms.ignite_utils.sql.dml.SelectCountSql;
+import com.github.microprograms.ignite_utils.sql.dml.SelectSql;
+import com.github.microprograms.ignite_utils.sql.dml.Where;
 import com.github.microprograms.micro_api_runtime.annotation.MicroApiAnnotation;
-import com.github.microprograms.micro_api_runtime.model.Response;
 import com.github.microprograms.micro_api_runtime.model.Request;
+import com.github.microprograms.micro_api_runtime.model.Response;
+import com.github.microprograms.micro_entity_definition_runtime.annotation.Comment;
 import com.github.microprograms.micro_entity_definition_runtime.annotation.Required;
+import com.github.microprograms.qipai_exchange_manager_api.utils.Consts;
 
 @Comment(value = "房卡订单 - 查询列表")
 @MicroApiAnnotation(type = "read", version = "v1.0.45")
@@ -13,14 +26,29 @@ public class RoomCardOrder_QueryList_Api {
     public static Response execute(Request request) throws Exception {
         Req req = (Req) request;
         Resp resp = new Resp();
+        PagerRequest pagerRequest = new PagerRequest(req.getPageIndex(), req.getPageSize());
+        try (Connection conn = IgniteUtils.getConnection(Consts.jdbc_url)) {
+            String finalCondition = buildFinalCondition(req);
+            ResultSet selectRs = conn.createStatement().executeQuery(new SelectSql(MixOrder.class).where(finalCondition).pager(pagerRequest).build());
+            resp.setData(IgniteUtils.getJavaObjectList(selectRs, MixOrder.class));
+            ResultSet selectCountRs = conn.createStatement().executeQuery(new SelectCountSql(MixOrder.class).where(finalCondition).build());
+            resp.setPager(new PagerResponse(pagerRequest, IgniteUtils.getCount(selectCountRs)));
+        }
         return resp;
+    }
+
+    private static String buildFinalCondition(Req req) {
+        ComplexCondition dtCreate = Where.and(Condition.build("dtCreate>", req.getSearchBeginTimestamp()), Condition.build("dtCreate<", req.getSearchEndTimestamp()));
+        ComplexCondition keyword = Where.and(LikeCondition.build("id", req.getSearchKeyword()), LikeCondition.build("goodsOrder_detail", req.getSearchKeyword()));
+        ComplexCondition transportReceiver = Where.and(LikeCondition.build("transportReceiver", req.getSearchTransportReceiver()), LikeCondition.build("transportPhone", req.getSearchTransportReceiver()));
+        Condition status = (req.getSearchStatus() == null || req.getSearchStatus() == 0) ? null : Condition.build("status=", req.getSearchStatus());
+        Condition orderType = Condition.build("orderType=", 2);
+        return Where.and(dtCreate, keyword, transportReceiver, status, orderType).toString();
     }
 
     public static class Req extends Request {
 
-        @Comment(value = "页码(从0开始)")
-        @Required(value = true)
-        private Integer pageIndex;
+        @Comment(value = "页码(从0开始)") @Required(value = true) private Integer pageIndex;
 
         public Integer getPageIndex() {
             return pageIndex;
@@ -30,9 +58,7 @@ public class RoomCardOrder_QueryList_Api {
             this.pageIndex = pageIndex;
         }
 
-        @Comment(value = "页大小")
-        @Required(value = true)
-        private Integer pageSize;
+        @Comment(value = "页大小") @Required(value = true) private Integer pageSize;
 
         public Integer getPageSize() {
             return pageSize;
@@ -42,9 +68,7 @@ public class RoomCardOrder_QueryList_Api {
             this.pageSize = pageSize;
         }
 
-        @Comment(value = "搜索 - 关键字(订单号/商品名/商品编号)")
-        @Required(value = false)
-        private String searchKeyword;
+        @Comment(value = "搜索 - 关键字(订单号/商品名/商品编号)") @Required(value = false) private String searchKeyword;
 
         public String getSearchKeyword() {
             return searchKeyword;
@@ -54,9 +78,7 @@ public class RoomCardOrder_QueryList_Api {
             this.searchKeyword = searchKeyword;
         }
 
-        @Comment(value = "搜索 - 收货人(收货人/手机号)")
-        @Required(value = false)
-        private String searchTransportReceiver;
+        @Comment(value = "搜索 - 收货人(收货人/手机号)") @Required(value = false) private String searchTransportReceiver;
 
         public String getSearchTransportReceiver() {
             return searchTransportReceiver;
@@ -66,9 +88,7 @@ public class RoomCardOrder_QueryList_Api {
             this.searchTransportReceiver = searchTransportReceiver;
         }
 
-        @Comment(value = "搜索 - 开始时间戳")
-        @Required(value = false)
-        private Long searchBeginTimestamp;
+        @Comment(value = "搜索 - 开始时间戳") @Required(value = false) private Long searchBeginTimestamp;
 
         public Long getSearchBeginTimestamp() {
             return searchBeginTimestamp;
@@ -78,9 +98,7 @@ public class RoomCardOrder_QueryList_Api {
             this.searchBeginTimestamp = searchBeginTimestamp;
         }
 
-        @Comment(value = "搜索 - 结束时间戳")
-        @Required(value = false)
-        private Long searchEndTimestamp;
+        @Comment(value = "搜索 - 结束时间戳") @Required(value = false) private Long searchEndTimestamp;
 
         public Long getSearchEndTimestamp() {
             return searchEndTimestamp;
@@ -90,9 +108,7 @@ public class RoomCardOrder_QueryList_Api {
             this.searchEndTimestamp = searchEndTimestamp;
         }
 
-        @Comment(value = "搜索 - 订单状态")
-        @Required(value = false)
-        private Integer searchStatus;
+        @Comment(value = "搜索 - 订单状态") @Required(value = false) private Integer searchStatus;
 
         public Integer getSearchStatus() {
             return searchStatus;
@@ -105,9 +121,7 @@ public class RoomCardOrder_QueryList_Api {
 
     public static class Resp extends Response {
 
-        @Comment(value = "房卡订单列表")
-        @Required(value = true)
-        private java.util.List<MixOrder> data;
+        @Comment(value = "房卡订单列表") @Required(value = true) private java.util.List<MixOrder> data;
 
         public java.util.List<MixOrder> getData() {
             return data;
@@ -117,9 +131,7 @@ public class RoomCardOrder_QueryList_Api {
             this.data = data;
         }
 
-        @Comment(value = "分页")
-        @Required(value = true)
-        private com.github.microprograms.ignite_utils.sql.dml.PagerResponse pager;
+        @Comment(value = "分页") @Required(value = true) private com.github.microprograms.ignite_utils.sql.dml.PagerResponse pager;
 
         public com.github.microprograms.ignite_utils.sql.dml.PagerResponse getPager() {
             return pager;
