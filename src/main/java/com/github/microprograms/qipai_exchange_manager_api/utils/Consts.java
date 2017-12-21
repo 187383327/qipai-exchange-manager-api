1de832527c9bd95d066a5ee05d44f953bfb1a5fe
xuzewei <xuzewei_2012@126.com>
2017-12-21 15:02:40 +0800

    可配置版本

--- src/main/java/com/github/microprograms/qipai_exchange_manager_api/utils/Consts.java
+++ src/main/java/com/github/microprograms/qipai_exchange_manager_api/utils/Consts.java
@@ -1,5 +1,13 @@
 package com.github.microprograms.qipai_exchange_manager_api.utils;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
 public class Consts {
-    public static String jdbc_url = "jdbc:ignite:thin://xzw1/public";
+    public static String jdbc_url;
+
+    static {
+        Config conf = ConfigFactory.load();
+        jdbc_url = conf.getString("jdbc_url");
+    }
 }
