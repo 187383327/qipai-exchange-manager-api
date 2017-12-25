02338c545c9a354c8459cfdf16d339851c01ef61
xuzewei <xuzewei_2012@126.com>
2017-12-25 17:42:06 +0800

    SampleData.java

--- src/main/java/com/github/microprograms/qipai_exchange_manager_api/sdk/SampleData.java
+++ src/main/java/com/github/microprograms/qipai_exchange_manager_api/sdk/SampleData.java
@@ -242,8 +242,7 @@ public class SampleData {
         req1.setType(2);
         req1.setName("Apple iPhone 8 64GB 金色 全网通4G手机");
         req1.setStock(999);
-        req1.setVipPrice(5000);
-        req1.setGoldVipPrice(4500);
+        req1.setPrice(15);
         req1.setPictures(JSON.toJSONString(Arrays.asList("https://img10.360buyimg.com/n1/s450x450_jfs/t9085/22/907696059/71305/93f88c62/59b85847N20776d8e.jpg")));
         req1.setDetail("这里是详情(富文本)");
         Goods_Add_Api.execute(req1);
@@ -252,8 +251,7 @@ public class SampleData {
         req2.setType(2);
         req2.setName("优它 YOTA YotaPhone2 俄罗斯双屏水墨屏智能手机电话 国礼手机 正品保证");
         req2.setStock(999);
-        req2.setVipPrice(3000);
-        req2.setGoldVipPrice(2500);
+        req1.setPrice(10);
         req2.setPictures(JSON.toJSONString(Arrays.asList("https://img12.360buyimg.com/n1/s450x450_jfs/t4036/222/34888999/70223/aa81730b/58390f1dN558d5940.jpg")));
         req2.setDetail("这里是详情(富文本)");
         Goods_Add_Api.execute(req2);
@@ -262,8 +260,7 @@ public class SampleData {
         req3.setType(2);
         req3.setName("亚奥星 迷你三防手机 军工老人手机 超小电霸路虎户外长待机 双卡双待移动联通 黑色");
         req3.setStock(999);
-        req3.setVipPrice(450);
-        req3.setGoldVipPrice(400);
+        req1.setPrice(1);
         req3.setPictures(JSON.toJSONString(Arrays.asList("http://img13.360buyimg.com/n1/s450x450_jfs/t13945/189/147124321/61733/584f84c1/5a0478ffN9ac6edb1.jpg")));
         req3.setDetail("这里是详情(富文本)");
         Goods_Add_Api.execute(req3);
@@ -272,8 +269,7 @@ public class SampleData {
         req4.setType(2);
         req4.setName("黑莓（BlackBerry） 【全球购】BlackBerry/黑莓 Q10手机 黑色");
         req4.setStock(999);
-        req4.setVipPrice(950);
-        req4.setGoldVipPrice(900);
+        req1.setPrice(7);
         req4.setPictures(JSON.toJSONString(Arrays.asList("http://img13.360buyimg.com/n5/s450x450_jfs/t3181/363/6439995132/73170/fc296ee1/58a6a68fN23fa0310.jpg")));
         req4.setDetail("这里是详情(富文本)");
         Goods_Add_Api.execute(req4);
@@ -282,8 +278,7 @@ public class SampleData {
         req5.setType(2);
         req5.setName("金柏利 001 移动/联通 双卡双待金属超薄卡片手机 学生儿童迷你小手机 黑色");
         req5.setStock(999);
-        req5.setVipPrice(450);
-        req5.setGoldVipPrice(400);
+        req1.setPrice(2);
         req5.setPictures(JSON.toJSONString(Arrays.asList("http://img11.360buyimg.com/n1/s450x450_jfs/t5281/145/77907999/147814/21568a1e/58f840aaNe7ccf8fc.jpg")));
         req5.setDetail("这里是详情(富文本)");
         Goods_Add_Api.execute(req5);
