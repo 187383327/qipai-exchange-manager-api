8e31fb7bb96fb93935d74c8881af12d655c9a806
xuzewei <xuzewei_2012@126.com>
2017-12-28 19:48:06 +0800

    Goods_UpdateRichText_Api.java

--- src/main/java/com/github/microprograms/qipai_exchange_manager_api/public_api/Goods_UpdateRichText_Api.java
+++ src/main/java/com/github/microprograms/qipai_exchange_manager_api/public_api/Goods_UpdateRichText_Api.java
@@ -1,10 +1,17 @@
 package com.github.microprograms.qipai_exchange_manager_api.public_api;
 
-import com.github.microprograms.micro_entity_definition_runtime.annotation.Comment;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+
+import org.apache.commons.io.IOUtils;
+
 import com.github.microprograms.micro_api_runtime.annotation.MicroApiAnnotation;
-import com.github.microprograms.micro_api_runtime.model.Response;
 import com.github.microprograms.micro_api_runtime.model.Request;
+import com.github.microprograms.micro_api_runtime.model.Response;
+import com.github.microprograms.micro_entity_definition_runtime.annotation.Comment;
 import com.github.microprograms.micro_entity_definition_runtime.annotation.Required;
+import com.github.microprograms.qipai_exchange_manager_api.utils.Consts;
 
 @Comment(value = "商品/优选商品 - 更新富文本")
 @MicroApiAnnotation(type = "read", version = "v1.0.62")
@@ -13,14 +20,22 @@ public class Goods_UpdateRichText_Api {
     public static Response execute(Request request) throws Exception {
         Req req = (Req) request;
         Response resp = new Response();
+        File dir = new File(Consts.goods_detail_dir);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        Writer writer = new FileWriter(new File(dir, req.getGoodsId()));
+        try {
+            IOUtils.write(req.getDetail(), writer);
+        } finally {
+            IOUtils.closeQuietly(writer);
+        }
         return resp;
     }
 
     public static class Req extends Request {
 
-        @Comment(value = "Token")
-        @Required(value = true)
-        private String token;
+        @Comment(value = "Token") @Required(value = true) private String token;
 
         public String getToken() {
             return token;
@@ -30,9 +45,7 @@ public class Goods_UpdateRichText_Api {
             this.token = token;
         }
 
-        @Comment(value = "商品ID")
-        @Required(value = true)
-        private String goodsId;
+        @Comment(value = "商品ID") @Required(value = true) private String goodsId;
 
         public String getGoodsId() {
             return goodsId;
@@ -42,9 +55,7 @@ public class Goods_UpdateRichText_Api {
             this.goodsId = goodsId;
         }
 
-        @Comment(value = "详情(富文本)")
-        @Required(value = true)
-        private String detail;
+        @Comment(value = "详情(富文本)") @Required(value = true) private String detail;
 
         public String getDetail() {
             return detail;
--- src/main/java/com/github/microprograms/qipai_exchange_manager_api/utils/Consts.java
+++ src/main/java/com/github/microprograms/qipai_exchange_manager_api/utils/Consts.java
@@ -5,9 +5,11 @@ import com.typesafe.config.ConfigFactory;
 
 public class Consts {
     public static String jdbc_url;
+    public static String goods_detail_dir;
 
     static {
         Config conf = ConfigFactory.load();
         jdbc_url = conf.getString("jdbc_url");
+        goods_detail_dir = conf.getString("goods_detail_dir");
     }
 }
