373d9c6a3b6272ef58ed63eef64539c7e768706f
xuzewei <xuzewei_2012@126.com>
2017-12-28 16:44:50 +0800

    SampleData.java

--- src/main/java/com/github/microprograms/qipai_exchange_manager_api/sdk/SampleData.java
+++ src/main/java/com/github/microprograms/qipai_exchange_manager_api/sdk/SampleData.java
@@ -41,7 +41,7 @@ import com.github.microprograms.qipai_exchange_manager_api.utils.Consts;
 
 public class SampleData {
     public static void main(String[] args) throws Exception {
-        // addBaseDate();
+        addBaseDate();
         // addExtDate();
         // updateUserHeadCoinAmount("893351", 200);
         // updateUserHeadCoinAmount("790942", 200);
